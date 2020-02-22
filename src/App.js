0d7d5d3e18a4cc8fa5a828a27fd13f5648b61129
Tim Finnigan <tim.finnigan1@gmail.com>
2020-02-22 11:25:39 -0500

    put table into separate component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React from "react";
-import Table from "react-bootstrap/Table";
+import NewsletterComponent from "./components/newsletterTable.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@jacob</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
+        <NewsletterComponent />
       </header>
     </div>
   );
---
+++ src/components/newsletterTable.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Table from "react-bootstrap/Table";
+
+class NewsletterComponent extends Component {
+  render() {
+    return (
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@jacob</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    );
+  }
+}
+export default NewsletterComponent;
