ab1108bafd41629a2e1ef50245799970ccc81c3d
Tim Finnigan <tim.finnigan1@gmail.com>
2020-03-02 08:26:25 -0500

    add todo items

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           {
             Header: " ",
             accessor: "image",
-            Cell: ({ cell: { value } }) => <img class="icon" src={value} />
+            Cell: ({ cell: { value } }) => <img class="icon" src={value} alt=""/>
           },
           {
             Header: "Name",
@@ -67,6 +67,15 @@ function App() {
 
   return (
     <div className="App">
+      <span>TODO</span>
+      <ol>
+        <li>randomize list on page load</li>
+        <li>add google analytics</li>
+        <li>add your own newletter to the top (mail chimp?)</li>
+        <li>post on product hunt</li>
+        <li>add an about page/contact form</li>
+        <li>upon launching - thank people on Twitter</li>
+      </ol>
       <h1>newsletters.best</h1>
       <Table columns={columns} data={newsletters} />
     </div>
