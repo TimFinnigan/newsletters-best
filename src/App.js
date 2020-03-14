ad84e006ce4df3b4f745a1aaec137a17d3aaf497
Tim Finnigan <tim.finnigan1@gmail.com>
2020-03-14 18:22:24 -0400

    update todo list

--- src/App.js
+++ src/App.js
@@ -71,16 +71,11 @@ function App() {
     <div className="App">
       <span>TODO</span>
       <ol>
-        <li>randomize list on page load</li>
         <li>add google analytics</li>
         <li>add your own newletter to the top (mail chimp?)</li>
         <li>post on product hunt</li>
         <li>add an about page/contact form</li>
-        <li>upon launching - thank people on Twitter</li>
-        <li>
-          hide Description column on Mobile/small views? (or convert to a button
-          you can click to see popover of details!)
-        </li>
+        <li>upon launching - thank people on Twitter (also helps spread the word...)</li>
       </ol>
       <h1>newsletters.best</h1>
       <Table
